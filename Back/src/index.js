8bbaf5faa71a2b4a8154cf4f0e3f727875a65544
Rafael Levi <rafaellevissa@gmail.com>
2020-11-24 10:19:22 -0300

    cors add

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('./docs/doc.json');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
